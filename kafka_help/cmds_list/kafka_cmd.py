b12ce94efe1d143d82d7bdd4c5270f3976e225e6
LBubbly <126142385+LBubbly@users.noreply.github.com>
2024-03-05 11:00:17 -0800

    Add files via upload

---
+++ kafka_help/cmds_list/kafka_cmd.py
@@ -0,0 +1,59 @@
+import subprocess as sbp
+
+class KafkaCmd:
+    def __init__(self):
+        '''
+        Might Need...
+        
+        --bin/__.sh
+        --bootstrap-server localhost:9092,localhost:9093
+        --replication-factor 1-3
+        --partitions #
+        --topic topic1,topic2
+        
+        
+        '''
+        pass
+    
+    '''
+    Main Methods:
+    
+    - Start ZooKeeper
+    - Start Kafka Broker
+    - Create Topic 
+    - List Topics
+    - ...Topic Details
+    - Start Producer
+    - Start Consumer (Option to read from the beginning... or not)
+    - List Consumer Groups
+    - ...Consumer Group Details
+    
+    **MAKE SURE YOU PRINT EACH COMMAND IN THE CONSOLE!!!**
+    
+    '''
+    
+    
+    def create_topic_s(self):
+        pass
+    
+    def list_topics(self):
+        pass
+    
+    def topic_details(self):
+        pass
+    
+    def create_partition_s(self):
+        pass
+    
+    def view_partition(self):
+        pass
+    
+    def create_consumer_group_s(self):
+        pass
+    
+    def list_consumer_groups(self):
+        pass
+    
+    def create_consumer_s(self):
+        pass
+    
